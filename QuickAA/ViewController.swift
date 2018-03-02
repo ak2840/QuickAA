759653a838e5852d1e873214e379b2481201c50a
Tony <ak28402840@gmail.com>
2018-03-02 17:30:36 +0800

    Project init

---
+++ QuickAA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickAA
+//
+//  Created by Tony on 2018/3/2.
+//  Copyright © 2018年 ak2840. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
